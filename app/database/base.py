db9373c367facad775d0b7ef6ec5546246effec5
kokilavani.p <kokila63vani@gmail.com>
2026-09-19 13:24:02 +0530

    declartiv meta fixes

--- app/database/base.py
+++ app/database/base.py
@@ -1,4 +1,4 @@
-from  sqlalchemy.orm import DeclarativeMeta
+from  sqlalchemy.orm import DeclarativeBase
 
-class Base(DeclarativeMeta):
+class Base(DeclarativeBase):
     pass
--- app/main.py
+++ app/main.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI
-from  app.database.base import  Base
-from  app.database.session import engine
+from app.database.base import Base
+from app.database.session import engine
 
 
-# create  a instance of FastAPI
 app = FastAPI(
-    title ="my first e-commerce project API",
-    description = "This is a simple e-commerce project API built with FastAPI",
-    version = "1.0.0",
+    title="my first e-commerce project API",
+    description="This is a simple e-commerce project API built with FastAPI",
+    version="1.0.0",
 )
 
 
 Base.metadata.create_all(bind=engine)
+
 
 @app.get("/")
 def home():
